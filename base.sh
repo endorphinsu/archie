3ee52189d60242fa8c4d9c15d9f08a84750f7be1
endorphinsu <66004932+endorphinsu@users.noreply.github.com>
2021-01-11 09:03:47 +0200

    Update base.sh

--- base.sh
+++ base.sh
@@ -1,92 +1,100 @@
 #!/usr/bin/env bash
 
-# Goal: No brainer KISS script
-# Todo make this readable to people who don't know bash and any of these commands if possible
-# Add comments
-# Make an interactive script?
+# UEFI and ethernet systems only
 
-region=Europe
-city=Vilnius
-myusername=you
-myhostname=archie
-ucode=intel-ucode
-drive=/dev/sda
+# add ability to load settings from a file
 
-# -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ #
+DIALOG='dialog --cursor-off-label --colors --no-mouse'
+DIALOGSIZE='0 0'
 
-for number in {1..9}   
-do
-parted $drive -- rm "$drive"$number                           
-done                                
+nohup timedatectl set-ntp true > /dev/null 2>&1 &
 
-parted -s $drive mklabel gpt
+# Redirect INPUT to STDOUT
+exec 3>&1
 
-parted -s -a optimal $drive -- mkpart ESP fat32 1Mib 512Mib
+DRIVES=$(lsblk -rpo "name,type,size,mountpoint" | grep 'disk' | awk '$4==""{printf "%s (%s)\n",$1,$3}')
 
-parted -s -a optimal $drive -- mkpart primary 512MiB 100%
+DRIVE=$($DIALOG --title 'Choose drive to partition' --menu "" $DIALOGSIZE 0 $DRIVES 2>&1 1>&3)
 
-# -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ #
+parted -s $DRIVE -- mklabel gpt
 
-mkfs.f2fs -f "$drive"2
-mkfs.fat -F 32 "$drive"1
+parted -s -a optimal "$DRIVE"1 -- mkpart ESP fat32 1Mib 512Mib
 
-mount "$drive"2 /mnt
+parted -s -a optimal "$DRIVE"2 -- mkpart primary 512MiB 100%
+
+mkfs.f2fs -f $DRIVE
+
+mount "$DRIVE"2 /mnt
+
 mkdir /mnt/boot
-mount "$drive"1 /mnt/boot
 
-# -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ #
+mount "$DRIVE"1 /mnt/boot
 
-timedatectl set-ntp true
+CPU=$($DIALOG --checklist "Check your cpu microcode:" $DIALOGSIZE 3 intel-ucode "" 1 amd-ucode "" 2 2>&1 1>&3)
 
 reflector --verbose --latest 200 --sort score --save /etc/pacman.d/mirrorlist
-cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 
-pacstrap /mnt base base-devel linux linux-firmware dhcpcd grub efibootmgr $ucode
+clear
+pacstrap /mnt base base-devel linux linux-firmware dhcpcd grub efibootmgr $CPU
 
-genfstab -U /mnt >> /mnt/etc/fstab
+USER=$($DIALOG --inputbox 'Enter your username:' $DIALOGSIZE 2>&1 1>&3)
 
-arch-chroot /mnt systemctl start dhcpcd
-
-ln -sf /mnt/usr/share/zoneinfo/$region/$city /mnt/etc/localtime
-
-arch-chroot /mnt hwclock --systohc
-
-sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /mnt/etc/locale.gen
-
-echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
-
-arch-chroot /mnt locale-gen
-
-echo "$myhostname" > /mnt/etc/hostname
-
-echo "127.0.0.1	localhost
-::1		localhost
-127.0.1.1	$myhostname.localdomain	$myhostname
-" >> /mnt/etc/hosts
-
-sed -i 's/#Color/Color\nILoveCandy/g' /mnt/etc/pacman.conf
-
-sed -i '/^#\[multilib\]/{s/^#//;n;s/^#//;n;s/^#//}' /etc/pacman.conf
-
-# -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ #
-
-arch-chroot /mnt useradd -m -G wheel,storage,power,video,audio,rfkill,input $myusername
-
-arch-chroot /mnt passwd
-arch-chroot /mnt passwd $myusername
+arch-chroot /mnt useradd -m -G wheel,storage,power,video,audio,rfkill,input $USER
 
 sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /mnt/etc/sudeors
 
 echo -e "\nDefaults insults" >> /mnt/etc/sudoers
 
-# -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ #
+HOST=$($DIALOG --inputbox 'Enter your hostname:' $DIALOGSIZE 2>&1 1>&3)
 
-arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+echo "$HOST" > /mnt/etc/hostname
 
-# Grub timeout and resolution
+echo "127.0.0.1 localhost  
+::1             localhost  
+127.0.1.1       $HOST.localdomain       $HOST  
+" >> /mnt/etc/hosts
 
-arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+REGIONS=$(ls -l /usr/share/zoneinfo/ | grep '^d' | sed 's/.* //g')
+REGION=$($DIALOG --no-items --title 'Choose your region' --menu "" $DIALOGSIZE 0 $REGIONS 2>&1 1>&3)
 
-umount -R /mnt
+CITIES=$(ls /usr/share/zoneinfo/$REGION)
+CITY=$($DIALOG --no-items --title 'Choose your region' --menu "" $DIALOGSIZE 0 $CITIES 2>&1 1>&3)
 
-reboot
+ln -sf /mnt/usr/share/zoneinfo/$REGION/$CITY /mnt/etc/localtime
+
+echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
+
+arch-chroot /mnt locale-gen
+
+arch-chroot /mnt hwclock --systohc
+
+
+if ($DIALOG --yesno "Enable multilib?" $DIALOGSIZE) then
+sed -i '/^#\[multilib\]/{s/^#//;n;s/^#//;n;s/^#//}' /mnt/etc/pacman.conf
+fi
+
+if ($DIALOG --yesno "Enable pacman eastereggs?" $DIALOGSIZE) then
+sed -i 's/#Color/Color\nILoveCandy/g' /mnt/etc/pacman.conf
+fi
+
+THREADS=$(nproc)
+
+sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j'$THREADS'"/g' /mnt/etc/makepkg.conf
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt systemctl enable dhcpcd
+
+arch-chroot /mnt systemctl enable fstrim.timer
+
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=3/g' /mnt/etc/default/grub                                                                                                                                                                         
+                                                                                                                                                                                                                                         
+# sed resolution
+                     
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB                                                                                                                                             
+                                                                                                                                                                                                                                         
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg                                                                                                                                                                                    
+                                                                                                                                                                                                                                         
+umount -R /mnt                                                                                                                                                                                                                           
+                                                                                                                                                                                                                                         
+reboot       
