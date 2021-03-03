c8b7e3abf090eef4f84af0326b0a387966c07d8b
endorphinsu <66004932+endorphinsu@users.noreply.github.com>
2021-03-03 10:35:37 +0200

    Update depends.sh

--- depends.sh
+++ depends.sh
@@ -12,13 +12,13 @@ PKGS=(
 'xorg-server'
 'xorg-xinit'
 'xorg-server-devel'
-#'xorg-xrdb'
+'xorg-xrdb'
 
 # Browser
 'firefox'
 
 # Terminal
-#'kitty'
+'kitty'
 
 # Shell
 'zsh'
@@ -34,93 +34,90 @@ PKGS=(
 'pipewire'
 'pipewire-pulse'
 
-# DE
-#'gnome'
-'gnome-tweaks'
-'gdm'
-
 # WM
-#'i3-gaps'
-#'polybar'
-#'python-i3ipc'
-#'alternating-layouts-git'
+'i3-gaps'
+'polybar'
+'python-i3ipc'
+'alternating-layouts-git'
 
 # Utils
-#'xclip'
-#'maim'
-#'feh'
-#'rofi'
-#'fzf'
-#'mpc'
-#'mpd'
-#'ncmpcpp'
+'xclip'
+'maim'
+'feh'
+'rofi'
+'fzf'
+'mpc'
+'mpd'
+'ncmpcpp'
 
 # Lockscreen
-#'i3lock-color'
-#'xautolock'
+'i3lock-color'
+'xautolock'
 
 # Theme
 'papirus-icon-theme'
-#'lxappearance-gtk3'
-#'breeze-snow-cursor-theme'
-#'qt5ct'
-#'qt5-styleplugins'
+'lxappearance-gtk3'
+'breeze-snow-cursor-theme'
+'qt5ct'
+'qt5-styleplugins'
 
 # File Manager
-#'thunar'
-#'thunar-volman'
-#'thunar-archive-plugin'
-#'xarchiver'
-#'gvfs'
-#'gvfs-mtp'
-#'unzip'
-#'ffmpegthumbnailer'
-#'tumbler'
+'thunar'
+'thunar-volman'
+'thunar-archive-plugin'
+'xarchiver'
+'gvfs'
+'gvfs-mtp'
+'unzip'
+'ffmpegthumbnailer'
+'tumbler'
 
 # Misc
 'xdg-user-dirs'
-#'youtube-dl'
-#'python-pywal'
-#'redshift'
-#'ananicy'
-#'picom'
-#'profile-sync-daemon'
+'youtube-dl'
+'python-pywal'
+'redshift'
+'ananicy'
+'picom'
+'profile-sync-daemon'
 
 # Mirrorlist
 'chaotic-mirrorlist'
 'chaotic-keyring'
 
 )
-# Will probably ask for password, but you wont see it
 for PKG in "${PKGS[@]}"; do
     echo "INSTALLING: ${PKG}"
     paru -S "$PKG" --noconfirm --needed > /dev/null 2>&1
 done
 
-sudo chsh -s /bin/zsh
+chsh -s /bin/zsh
 
-#cd
-#xdg-user-dirs-update --force
-#rmdir Templates
-#rmdir Public
+cd
+xdg-user-dirs-update --force
+rmdir Templates
+rmdir Public
 
-#echo -e "\nEDITOR=nvim\n\nWINEESYNC=1\nWINEFSYNC=1\n\nQT_AUTO_SCREEN_SCALE_FACTOR=1\nQT_QPA_PLATFORMTHEME=qt5ct" > /etc/environment
+echo -e "\nEDITOR=nvim\n\nWINEESYNC=1\n\nQT_AUTO_SCREEN_SCALE_FACTOR=1\nQT_QPA_PLATFORMTHEME=qt5ct" | sudo tee -a /etc/environment
 
-#mkdir ~/.fonts 
-#cd ~/.fonts
-#curl -L --progress-bar https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FantasqueSansMono.zip > ~/.fonts/FantasqueSansMono.zip
-#unzip -o ~/.fonts/FantasqueSansMono.zip -d ~/.fonts
-#rm ~/.fonts/FantasqueSansMono.zip
-#rm ~/.fonts/*Compatible.ttf
-#fc-cache -f
+mkdir ~/.fonts 
+cd ~/.fonts
+curl -L --progress-bar https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FantasqueSansMono.zip > ~/.fonts/FantasqueSansMono.zip
+unzip -o ~/.fonts/FantasqueSansMono.zip -d ~/.fonts
+rm ~/.fonts/FantasqueSansMono.zip
+rm ~/.fonts/*Compatible.ttf
+fc-cache -f
 
-#systemctl enable --user mpd.service
-#systemctl enable --user psd.service
-#systemctl enable prelockd.service
-#systemctl enable ananicy.service
-#systemctl enable nohang.service
-systemctl enable gdm
+systemctl enable --user mpd.service
+systemctl enable --user psd.service
+systemctl enable prelockd.service
+systemctl enable ananicy.service
+systemctl enable nohang.service
 systemctl enable --user pipewire
 systemctl enable --user pipewire-pulse
 
-echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
+
+echo "#!/usr/bin/env bash\ni3" >> ~/.xinitrc
+
+sudo pacman -Syu
