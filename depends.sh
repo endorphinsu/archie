cec3b280e6ba4c3216a6f815cbf00ededaffdef8
endorphinsu <66004932+endorphinsu@users.noreply.github.com>
2021-03-20 16:11:01 +0200

    Update depends.sh

--- depends.sh
+++ depends.sh
@@ -91,7 +91,7 @@ PKGS=(
 'python-pywal'
 'redshift'
 'ananicy'
-'picom'
+'picom-git'
 'profile-sync-daemon'
 'mpv'
 'transmission-gtk'
@@ -183,7 +183,33 @@ echo 'kernel.printk = 3 3 3 3' | sudo tee -a /etc/sysctl.d/99-tweaks.conf
 
 sudo pywalfox install -g
 
+mkdir -p ~/.config/gtk-3.0/
+touch ~/.config/gtk-3.0/settings.ini
+
+echo '[Settings]
+gtk-theme-name=Theme
+gtk-icon-theme-name=Icon
+gtk-font-name=Cantarell 11
+gtk-cursor-theme-name=Breeze_Snow
+gtk-cursor-theme-size=0
+gtk-toolbar-style=GTK_TOOLBAR_BOTH_HORIZ
+gtk-toolbar-icon-size=GTK_ICON_SIZE_LARGE_TOOLBAR
+gtk-button-images=0
+gtk-menu-images=0
+gtk-enable-event-sounds=1
+gtk-enable-input-feedback-sounds=1
+gtk-xft-antialias=1
+gtk-xft-hinting=1
+gtk-xft-hintstyle=hintmedium' > ~/.config/gtk-3.0/settings.ini
+
+# Dots here
+
+chmod u+x -R ~/.config/scripts/
+
+# rice, to generate theme
 rm ~/.bash*
+
+rm -rf archie
 
 sudo pacman -Syu
 
